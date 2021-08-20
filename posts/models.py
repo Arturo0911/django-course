7ac75819319491102fa2957ed947fa054b785d2b
Arturo0911 <anegreiross@outlook.com>
2021-08-20 11:34:22 -0500

    ok

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+"""Posts models."""
+
+# Django
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+    email = models.EmailField()
+    password = models.CharField()
+
+    first_name = models.CharField()
+    last_name = models.CharField()
+    bio = models.TextField()
+
+    birthdate  = models.DateField()
+
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
