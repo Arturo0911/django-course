0fda840eeeafc1ec5f6f207c75b15926d0315f3f
Arturo0911 <anegreiross@outlook.com>
2021-08-20 22:10:32 -0500

    ok

--- posts/models.py
+++ posts/models.py
@@ -7,14 +7,21 @@ from django.db import models
 
 class User(models.Model):
 
-    email = models.EmailField()
-    password = models.CharField()
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
 
-    first_name = models.CharField()
-    last_name = models.CharField()
-    bio = models.TextField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
-    birthdate  = models.DateField()
+    is_admin = models.BooleanField(default=False)
+    bio = models.TextField(blank=True)
+    birthdate  = models.DateField(blank=True, null=True)
 
-    created = models.DateTimeField()
-    modified = models.DateTimeField()
+    # When an insntance is creted automatically will be create 
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        """Return email"""
+        return self.email
